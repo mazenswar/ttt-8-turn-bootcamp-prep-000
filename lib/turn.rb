0034bb27fa17c297fa3e8e2b257d29d303511f90
Mazen AlSwar <mazenswar@protonmail.com>
2018-10-18 21:01:24 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -17,6 +17,10 @@ def position_taken?(board, index)
   
 
 def valid_move?(board, index)
-  
+  if !position_taken?(board, index) && index.between?(0, 8)
+    return true
+  else 
+    return false 
+  end 
 end 
 
